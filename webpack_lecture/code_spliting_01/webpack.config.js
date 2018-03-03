1de8b0946c0b4dbc09890dc5249f8e8332ba8809
Dongho Sim <dhsim86@gmail.com>
2018-03-03 15:22:17 +0900

    Added the plugins for libraries code splitting.
    Signed-off-by: Dongho Sim <dhsim86@gmail.com>

--- webpack_lecture/code_spliting_01/webpack.config.js
+++ webpack_lecture/code_spliting_01/webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: {
@@ -12,5 +13,15 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      // name: 'vendor' // Specify the common bundle's name.
+      names: ['vendor', 'manifest'] // Extract the webpack bootstrap logic into manifest.js
+    }),
+    new ManifestPlugin({
+      filename: 'manifest.json',
+      basepath: './dist/'
+    }),
+  ]
 }
