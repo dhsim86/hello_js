7a9721b0309d633d9e9725f3ca590e1ce744bb3f
Dongho Sim <dhsim86@gmail.com>
2019-12-04 15:01:15 +0900

    vue router navigation guard
    Signed-off-by: Dongho Sim <dhsim86@gmail.com>

--- vue_lecture/router_practice/app.js
+++ vue_lecture/router_practice/app.js
@@ -5,16 +5,36 @@ var Foo = {
 var Bar = {
   template: '<div>bar</div>'
 }
-
+/*
 var routes = [
   { path: '/foo', component: Foo },
   { path: '/bar', component: Bar }
 ]
-
+*/
 var router = new VueRouter({
-  routes
+  //routes
 })
+
+router.addRoutes([ { path: '/foo', component: Foo }])
+router.addRoutes([ { path: '/bar', component: Bar }])
 
 var app = new Vue({
   router
 }).$mount('#app')
+
+function checkLogin() {
+  return axios.get('http://localhost:10080/monitor/l7check');
+}
+
+router.beforeEach((to, from, next) => {
+  checkLogin().then(response => {
+    if (response.data == 'OK') {
+      next();
+    } else {
+      next();
+    }
+  }).catch(error => {
+    next();
+  });
+  //authenticationInquiryService.isAuthenticated$(to.params.appKey).subscribe(result => next(), e => toastLogin.gotoLoginPage())
+})
