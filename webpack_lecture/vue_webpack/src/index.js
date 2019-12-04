0760b265f319e02c0d2a86854ba54fa0d94c2a96
Dongho Sim <dhsim86@gmail.com>
2019-12-04 15:37:44 +0900

    Added vue-webpack
    Signed-off-by: Dongho Sim <dhsim86@gmail.com>

---
+++ webpack_lecture/vue_webpack/src/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App'
+
+const main = new Vue({
+  ...App
+})
+
+main.$mount('#app')
---
+++ webpack_lecture/vue_webpack/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+cwd = file => {
+  return path.join(process.cwd(), file || '')
+}
+
+module.exports = {
+  entry: {
+    src: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.css', '.json'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    },
+    modules: [cwd('node_modules'), cwd('src')]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '../dist'),
+    hot: true,
+    host: '0.0.0.0',
+    port: 4000,
+    disableHostCheck: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+       },
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.es6$/,
+        loaders: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: '',
+      template: path.resolve(__dirname, 'index.html'),
+      filename: path.join(__dirname, '../dist/index.html')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
