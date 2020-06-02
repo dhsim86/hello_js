2e6887a36f536370c41110a5dea61ef22bb51702
Dongho Sim <dhsim86@gmail.com>
2020-06-02 13:21:48 +0900

    Added instanceof sample.
    Signed-off-by: Dongho Sim <dhsim86@gmail.com>

---
+++ poiemaweb_lecture/js/5.type_checking/instanceof.js
@@ -0,0 +1,44 @@
+var Super = function(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+var Child = function(name, age, value) {
+  this.value = value;
+}
+
+Super.prototype.print = function() {
+  console.log(this.name);
+  console.log(this.value);
+}
+
+console.log(Super.prototype.constructor); // [Function: Super]
+
+console.log('-----------------------')
+
+console.log(Child.__proto__);           // [Function]
+console.log(Child.prototype.__proto__); // {}
+console.log(Child.prototype.constructor); // [Function: Child]
+
+child = new Child(10);
+console.log(child instanceof Super);  // false
+
+console.log('-----------------------')
+
+Child.prototype = Object.create(Super.prototype);
+
+console.log(Child.__proto__);           // [Function]
+console.log(Child.prototype.__proto__); // Super { print: [Function] }
+console.log(Child.prototype.constructor); // [Function: Super]
+
+child = new Child(10);
+console.log(child instanceof Super);  // true
+console.log(child instanceof Child);  // true
+
+console.log('-----------------------')
+
+Child.prototype.constructor = Child;
+
+child = new Child(10);
+console.log(child instanceof Super);  // true
+console.log(child instanceof Child);  // true
