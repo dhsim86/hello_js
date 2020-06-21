d369c12e6fd37aedb91609565e07cf3611f9fe22
Dongho Sim <dhsim86@gmail.com>
2020-06-21 23:07:32 +0900

    webpack sample

---
+++ poiemaweb_lecture/es6/10.webpack/src/js/main.js
@@ -0,0 +1,17 @@
+import { pi, power, Foo } from './lib'
+
+console.log(pi);
+console.log(power(pi, pi));
+
+const f = new Foo();
+console.log(f.foo());
+console.log(f.bar());
+
+// need polyfill
+
+console.log(new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 3000);
+}).then(console.log))
+
+console.log(Object.assign({}, { x: 1 }, { y: 2}));
+console.log(Array.from([1, 2, 3], v => v + v));
