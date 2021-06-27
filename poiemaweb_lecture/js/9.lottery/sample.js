69ac8251333d7d87752293df6ba72c3e936db5e7
Dongho Sim <dhsim86@gmail.com>
2021-06-27 11:18:02 +0900

    update lottery

--- poiemaweb_lecture/js/9.lottery/sample.js
+++ poiemaweb_lecture/js/9.lottery/sample.js
@@ -1,5 +1,16 @@
 var rng = window.crypto || window.msCrypto
 var array = new Uint8Array(6);
+var result;
+var count = 0;
 
-window.crypto.getRandomValues(array)
-console.log(array.map(num => (num % 45) + 1).sort().join(','));
+while (count < 5) {
+    window.crypto.getRandomValues(array)
+    result = Array.from(new Set(array.map(num => (num % 45) + 1).sort()));
+
+    if (result.length < 6) {
+        continue;
+    } else {
+        console.log(result.join(','));
+        count++;
+    }
+}
